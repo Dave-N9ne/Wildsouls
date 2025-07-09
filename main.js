1f2215977471502ebfd0f61c4f14b73c83d8b740
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-07-09 15:09:25 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function addNavMenu () {
+    const button = document.querySelector('#header-burger');
+    const nav = document.querySelector('.nav__list');
+    const body = document.body;
+    
+    button.addEventListener ('click', function (event) {
+        const elementsToActive = [button, nav];
+        elementsToActive.forEach(toggleActiveClass);
+        body.classList.toggle('lock');
+    })
+}
+
+function toggleActiveClass (element) {
+    element.classList.toggle('active');
+}
+
+addNavMenu ();
